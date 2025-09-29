66beaad4e79a1c966c2ddeed1e5c89ef45e2059a
ukduw <edhkmayo@gmail.com>
2025-09-29 15:45:59 -0400

    added func skeleton, general pseudocode, and notes/plans

--- support_resistance_detector.py
+++ support_resistance_detector.py
@@ -7,14 +7,36 @@
     # --header 'Apca-Api-Key-Id: <KEY>' \
     # --header 'Apca-Api-Secret-Key: <SECRET>' \
 
-# look into websocket, available data, etc...
+# get up to date list later...
+# transition to binance business account(?) in future?
 
-
-# declare list of coins
-# iterate over, api request current day bar data
-# detect sr levels
-# logic to determine closest ones to strategy parameters
-# return to use in parameter_writer
-
+# check above list of coins for ~50 - 200/300/500/1,000mil average volume?
+# alpaca - look into websocket, available data, etc...
 
 # don't forget pip freeze > requirements.txt
+
+
+coins = [] # fill later
+dollar_position_size = 2000
+
+bar_data = {}
+all_levels = {}
+levels = {}
+
+def level_detector():
+    # iterate over coins, api request current day bar data
+    # store in bar_data
+    # detect sr levels, append all_levels
+    # logic to determine ones closest to strategy parameters
+    # append those to levels, per coin
+    # add dollar_value key with dollar_position_size as value
+    # return levels for use in parameter_writer
+    return
+
+
+
+# output format:
+    #{
+        #{"COIN1": [100.0, 95.0], "COIN2": [234.0, 223.0], ...},
+        #"dollar_value": 2000.0
+    #}
