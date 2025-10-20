5d2ecbfacf51577394917b58faeb6a3038b1e42b
ukduw <edhkmayo@gmail.com>
2025-10-20 11:25:47 -0400

    added file write iterator for parameters dict

--- log_writer.py
+++ log_writer.py
@@ -7,4 +7,8 @@
 
 def log_writer(dict):
     with open("param-log/param_log.txt", "a") as file:
-        file.write(f"{}, {}, placeholder, {}, {}" + "\n")
+        for key in dict.parameters:
+            file.write(f"{key}: Entry {dict.parameters[key][0]}, Stop {dict.parameters[key][1]}" + "\n")
+
+
+
