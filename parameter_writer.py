ceff8fac646708c849ac673a4323faf9f9baa6c0
ukduw <edhkmayo@gmail.com>
2025-12-29 09:18:14 -0500

    updated todo/notes on edge case

--- parameter_writer.py
+++ parameter_writer.py
@@ -13,10 +13,10 @@ CONFIG_FILE = "configs.json" # placeholder - change to the config json in crypto
 def parameter_writer(dict):
     configs = []
 
-    # THERE IS RARE CASE WHERE ALL PARAMS WILL BE REMOVED, CAUSING THIS TO THROW ERROR:
-        # (might be fine since it'll just result in no new config being written...)
-        # (but then that would probably result in the trading script using old config...)
-        # after fixing, trading script also needs to be updated to not run if config happens to be empty
+    # THERE IS EDGE CASE WHERE ALL PARAMS WILL BE REMOVED
+        # update: wouldn't throw an error, just wouldn't write a new config as the for loop would run 0 times
+        # would result in trading script using old configs for new day...
+    # after fixing, trading script also needs to be updated to not run if config happens to be empty
     for symbol in dict["parameters"]:
         configs.append({"symbol": symbol, "entry_price": dict[symbol][0], "stop_loss": dict[symbol][1], "dollar_value": dict['dollar_value']})
 
